7e7d730c603f91ecff138343b418843f16a1f9ba
Nikita-Voloshko <65704273+Nikita-Voloshko@users.noreply.github.com>
2020-06-10 15:33:27 +0300

    Add files via upload

---
+++ Only numbers.py
@@ -0,0 +1,16 @@
+class Only_Numbers(Exception):
+    def __init__(self, text):
+        self.text = text
+user_list = []
+i = ''
+while i != "stop":
+    user = input('Введите число в список.')
+    try:
+        int(user)
+    except Only_Numbers:
+        print("Ошибка. Это не число")
+    else:
+        user_list.append(user)
+    finally:
+        i = input('Для выхода ведите "stop".')
+print(user_list)
